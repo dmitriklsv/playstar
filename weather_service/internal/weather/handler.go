0d80670162651a3f2c801e7e7beaa9c8636f0c43
dmitri <dmitriklsv@gmail.com>
2022-03-07 14:23:30 +0600

    implemented weather handler

--- weather_service/internal/weather/handler.go
+++ weather_service/internal/weather/handler.go
@@ -2,13 +2,39 @@ package weather
 
 import (
 	"context"
+	"time"
 
+	apiclients "github.com/Levap123/playstar-test/weather_service/internal/api_clients"
 	"github.com/Levap123/playstar-test/weather_service/proto"
 )
 
 type WeatherHandler struct {
 	proto.UnimplementedWeatherServiceServer
+	cc *apiclients.CityClient
+	wc *apiclients.WeatherClient
 }
 
 func (h *WeatherHandler) GetWeather(ctx context.Context, req *proto.GetWeatherRequest) (*proto.GetWeatherResponse, error) {
+	cityCtx, cityCancel := context.WithTimeout(ctx, time.Second)
+	defer cityCancel()
+
+	city, err := h.cc.GetCityByCoordinates(cityCtx, req.Latitude, req.Longitude)
+	if err != nil {
+		return nil, err
+	}
+
+	weatherCtx, weatherCancel := context.WithTimeout(ctx, time.Second)
+	defer weatherCancel()
+
+	weather, err := h.wc.GetWeather(weatherCtx, city)
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.GetWeatherResponse{
+		Temperature:              weather.Temperature,
+		TemperatureApparent:      weather.Temperature,
+		Humidity:                 weather.Humidity,
+		PrecipitationProbability: weather.PrecipitationProbability,
+	}, nil
 }
