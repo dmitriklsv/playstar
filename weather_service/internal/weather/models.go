eefbc7b314f0ec72f91c836c318de3cd72bcfe6e
dmitri <dmitriklsv@gmail.com>
2023-03-07 13:13:18 +0600

    added coordinates model and mapping from request

--- weather_service/internal/weather/models.go
+++ weather_service/internal/weather/models.go
@@ -1,8 +1,22 @@
 package weather
 
+import "github.com/Levap123/playstar-test/weather_service/proto"
+
 type Weather struct {
 	Temperature              float32 `json:"temperature"`
 	TemperatureApparent      float32 `json:"temperatureApparent"`
 	Humidity                 uint64  `json:"humidity"`
 	PrecipitationProbability uint64  `json:"precipitationProbability"`
+}
+
+type Coordinates struct {
+	Latitude  float32
+	Longitude float32
+}
+
+func FromProtoRequestToCoordinates(req *proto.GetWeatherRequest) Coordinates {
+	return Coordinates{
+		Latitude:  req.Latitude,
+		Longitude: req.Longitude,
+	}
 }
