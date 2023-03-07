03bb6b109091aca3326d7716c2c9c06a3338000d
dmitri <dmitriklsv@gmail.com>
2023-03-07 13:21:22 +0600

    added city client

--- weather_service/internal/api_clients/city_client.go
+++ weather_service/internal/api_clients/city_client.go
@@ -1,1 +1,25 @@
 package apiclients
+
+import (
+	"context"
+
+	"github.com/Levap123/playstar-test/weather_service/proto"
+)
+
+type CityClient struct {
+	cl proto.CityServiceClient
+}
+
+func (cc *CityClient) GetCityByCoordinates(ctx context.Context, latitude, longitude float32) (string, error) {
+	req := &proto.GetCityRequest{
+		Latitude:  latitude,
+		Longitude: longitude,
+	}
+
+	resp, err := cc.cl.GetCity(ctx, req)
+	if err != nil {
+		return "", err
+	}
+
+	return resp.City, nil
+}
