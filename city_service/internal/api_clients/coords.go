1faab6b92633a4f63cd21bdc8e66287e1a3d9f26
dmitri <dmitriklsv@gmail.com>
2023-03-07 11:47:33 +0600

    added GetCityFromCoordiantes function in coordinates api clinet

--- city_service/internal/api_clients/coords.go
+++ city_service/internal/api_clients/coords.go
@@ -1,6 +1,13 @@
 package apiclients
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/Levap123/playstar-test/city_service/internal/city"
+)
 
 type CoordinatesClient struct {
 	httpClient *http.Client
@@ -12,6 +19,29 @@ func NewCoordinatesClient(cl *http.Client) *CoordinatesClient {
 	}
 }
 
-func (cc *CoordinatesClient) GetCityFromCoordiantes(latitude, longitude float32) {
-	
+func (cc *CoordinatesClient) GetCityFromCoordiantes(latitude, longitude float32) (city.City, error) {
+	URL := fmt.Sprintf(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=%f&longitude=%f&localityLanguage=en`,
+		latitude, longitude)
+	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	if err != nil {
+		return city.City{}, fmt.Errorf("coordinates client - get city - new request - %w", err)
+	}
+
+	resp, err := cc.httpClient.Do(req)
+	if err != nil {
+		return city.City{}, fmt.Errorf("coordinates client - get city - do request - %w", err)
+	}
+	defer resp.Body.Close()
+
+	reqBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return city.City{}, fmt.Errorf("coordinates client - get city - read request - %w", err)
+	}
+
+	var cityResp city.City
+	if err := json.Unmarshal(reqBytes, &cityResp); err != nil {
+		return city.City{}, fmt.Errorf("coordinates client - get city - unamrshal - %w", err)
+	}
+
+	return cityResp, nil
 }
